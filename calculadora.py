5802f5589b3b1971420689ecf83cf7caff17f914
wilson <wilsonisaiaspenamendez@gmail.com>
2026-07-23 17:49:32 -0400

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # esta parte es la encargada de invocar la calculadora
 import sys
+# sys.path.append() es el encargado de decirle a python en donde encontrar los modulos importados en este codigo(las clases y funciones)🤔🤔🤔
 sys.path.append("/usr/local/bin")
 from principal import Motor
 from ladrillos import Datos
