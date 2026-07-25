caf376d00123e6b1a22cd84e2ed66ff71515ef03
wilsonisaiaspenamendez <wilsonisaiaspenamendez@gmail.com>
2026-07-25 12:28:36 -0400

    calculadora media basica

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 # esta parte es la encargada de invocar la calculadora
-import sys
-sys.path.append("/usr/local/bin")
 from principal import Motor
 from ladrillos import Datos
 from result import Calculador
